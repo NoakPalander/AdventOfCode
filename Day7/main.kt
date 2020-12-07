feb8f3fc5058f10c4c6a78fc7167851bb7b9d40f
0xVali <noak.palander@protonmail.com>
2020-12-07 23:35:35 +0100

    cleanup day7

--- Day7/main.kt
+++ Day7/main.kt
@@ -1,43 +1,50 @@
 import java.io.File
 
-fun Part1(input: List<String>): Int {
-    val bags = input.filterNot { it.contains("no other") }.map {
-        val (color, content) = it.split(" bags contain ")
-        color to content.split(", ").map {
-            val items = it.split(" ")
-            items[0].toInt() to items.drop(1).dropLast(1).joinToString(" ")
+fun Construct(input: List<String>): Map<String, List<Pair<Int, String>>> {
+    return input.filterNot { rule ->
+        // Filters out all empty bags (dead ends)
+        rule.contains("no other")
+    }.map { validRule ->
+        // Separates the base color and the contents
+        val (color, contents) = validRule.split(" bags contain ")
+        color to contents.split(", ").map { children ->
+            val items = children.split(" ")
+            val childrenColors = items.drop(1).dropLast(1).joinToString(" ")
+
+            // Maps the count to the child colors
+            items[0].toInt() to childrenColors
         }
     }.toMap()
-
-    fun parentBags(bag: String): List<String> {
-        return bags.filter {
-            it.value.filter { p ->
-                p.second == bag
-            }.count() > 0
-        }.map {
-            parentBags(it.key)
-        }.flatten().plus(bag).distinct()
-    }
-
-    return parentBags("shiny gold").distinct().size - 1
 }
 
-fun Part2(input: List<String>): Int {
-    val bags = input.filterNot {
-        it.contains("no other")
-    }.map {
-        val (color, content) = it.split(" bags contain ")
-        color to content.split(", ").map {
-            val items = it.split(" ")
-            items[0].toInt() to items.drop(1).dropLast(1).joinToString(" ")
-        }
-    }.toMap()
+fun Part1(data: List<String>): Int {
+    val bags = Construct(data)
 
-    fun nestedBags(color: String): Int {
-        return 1 + (bags[color]?.sumBy { nestedBags(it.second) * it.first } ?: 0)
+    fun parse(bag: String): List<String> {
+        val found = bags.filter { parent ->
+            // Filters out all occurrences that doesn't contain golds
+            parent.value.filter { pair ->
+                pair.second == bag
+            }.count() > 0
+        }.map { parent ->
+            parse(parent.key)
+        }
+
+        // Converts the array to 1D, adds the original bag item, and filters out duplicates
+        return found.flatten().plus(bag).distinct()
     }
 
-    return nestedBags("shiny gold")  - 1
+    return parse("shiny gold").distinct().size - 1
+}
+
+fun Part2(data: List<String>): Int {
+    val bags = Construct(data)
+
+    fun parse(bagColor: String): Int {
+        return 1 + (bags[bagColor]?.sumBy { parse(it.second) * it.first } ?: 0)
+    }
+
+    return parse("shiny gold") - 1
 }
 
 fun main(args: Array<String>) {
